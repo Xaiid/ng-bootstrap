715dd01aae5fdf65d6af53bab9bcc17bf9fa0554
Jeaneth Farmer <xaid27@gmail.com>
2017-07-24 13:30:36 -0500

    style(modal): Added modal animations

--- misc/validate-commit.install.js
+++ misc/validate-commit.install.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var path = require('path');
 
 var hookInputPath = 'misc/validate-commit.js';
-var hookOutputPath = '.git/hooks/commit-msg';
+var hookOutputPath = 'git/hooks/commit-msg';
 
 if (!fs.existsSync(hookOutputPath)) {
   fs.writeFileSync(hookOutputPath, fs.readFileSync(hookInputPath));
